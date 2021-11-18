35752821d4f61ee59efd8ddf512ffc830e4ae388
Kurt F <67562815+fk-pixel@users.noreply.github.com>
2021-11-18 16:56:13 +0100

    Add files via upload

---
+++ api/routes/user.js
@@ -0,0 +1,12 @@
+const router = require("express").Router();
+const userControllers = require("../controllers/user");
+
+router.post("/user", userControllers.login);
+
+router.get("/user/:id", userControllers.getUser);
+
+router.put("/user", userControllers.updateUser);
+
+router.post("/user-create", userControllers.createUser);
+
+module.exports = router;
